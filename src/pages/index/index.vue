a158865ec27e17fa7fd69aa817cf87bf31e20e93
eidam1994 <eidam1994@outlook.com>
2022-04-26 17:48:42 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <view class="content">
+
+  </view>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {
+      title: 'Hello'
+    }
+  },
+  onLoad() {
+    uni.onTabBarMidButtonTap(() => {
+      uni.navigateTo({url: "/pages/index/index3"})
+    })
+  },
+  methods: {
+    goToPage() {
+      uni.navigateTo({url: "/pages/index/index2"})
+    }
+  }
+});
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
